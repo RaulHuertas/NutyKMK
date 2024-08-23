866a0859b16ba0c89f49917c595b4bdf55cf9d97
Raul Huertas <rax20037@gmail.com>
2024-08-23 14:50:07 -0500

    Pimoroni funcionando en modo usb

--- NUTYR/code.py
+++ NUTYR/code.py
@@ -6,25 +6,18 @@ import busio
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners import DiodeOrientation
 from kmk.modules.split import Split, SplitSide,SplitType
-import busio
-#from snKeycodes import ESKeycodes
 from kmk.extensions.media_keys import MediaKeys
-from keyAssignations import assignKeys
 from kmk.modules.layers import Layers
-# from kmk.modules.mouse_keys import MouseKeys
+from kmk.modules.mouse_keys import MouseKeys
 from kmk.scanners.keypad import MatrixScanner
 
-
-import board
-from kmk.hid import HIDModes
-
-from kmk.modules.power import Power
+from kmk.modules.pimoroni_trackball import Trackball
 
 col_pins = (board.P0_31,board.P0_29,board.P0_02,board.P1_15, board.P1_13,board.P1_11,)
 row_pins = (board.P0_10,board.P0_09,board.P1_07 ,board.P1_02,)
 
 diode_orientation = DiodeOrientation.ROW2COL
-class MyKeyboard(KMKKeyboard):
+class NutyKeyboard(KMKKeyboard):
     def __init__(self):
         # create and register the scanner
         self.matrix = MatrixScanner(
@@ -34,12 +27,9 @@ class MyKeyboard(KMKKeyboard):
             # optional arguments with defaults:
             columns_to_anodes=diode_orientation,
             interval=0.020,  # Debounce time in floating point seconds
-            max_events=64
+            max_events=2
         )
-keyboard = MyKeyboard()
-
-
-
+keyboard = NutyKeyboard()
 
 keyboard.coord_mapping =  [
     0,  1,  2,  3,  4,  5, 
@@ -52,38 +42,45 @@ keyboard.coord_mapping =  [
     42, 43, 44, 45, 46, 47,
 ]
 
-
 split = Split(
     split_side=SplitSide.RIGHT,
     #split_side=None,
-    split_type=SplitType.BLE,
+    split_type=SplitType.UART,
     split_target_left=False,
-    data_pin = board.P0_08,#RX
-    data_pin2 = board.P0_06,#TX
+    data_pin = board.P1_04,#RX
+    data_pin2 = board.P1_06,#TX
     uart_flip = False,
-    debug_enabled = True
+    debug_enabled = False
 )
 
+i2c = busio.I2C(scl=board.P0_06, sda=board.P0_08)
 
-power = Power(board.P0_13)
-power.enable = True
-power.enable_powersave(keyboard)
+trackball = Trackball(
+    i2c,
+    angle_offset=270,
+    
+)
+trackball.set_rgbw(200, 0, 255, 0)
 
 keyboard.extensions.append(MediaKeys())
+# keyboard.extensions.append(rgb)
+
 keyboard.modules = [
     split, 
     Layers(), 
-    power,
-    # MouseKeys()
+ #   power,
+    MouseKeys(),
+    trackball,
     #keyboard.modules.append(holdtap),
 ]
 
+from keyAssignations import assignKeys
 keyboard.keymap = assignKeys()
 keyboard.debug_enabled = True
 keyboard.powersave_pin = board.P0_13
 
 if __name__ == '__main__':
-    keyboard.go(hid_type=HIDModes.BLE)
-
+    #keyboard.go(hid_type=HIDModes.BLE)
+    keyboard.go()
 
 
